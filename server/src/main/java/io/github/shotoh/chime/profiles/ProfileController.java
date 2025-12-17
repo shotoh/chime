a4ca9667d8f923ca4307e717ac06ad7b62a5ac5c
shotoh <trueshotoh@gmail.com>
2025-12-16 23:26:12 -0800

    move profile calls to service

--- server/src/main/java/io/github/shotoh/chime/profiles/ProfileController.java
+++ server/src/main/java/io/github/shotoh/chime/profiles/ProfileController.java
@@ -1,19 +1,14 @@
 package io.github.shotoh.chime.profiles;
 
-import io.github.shotoh.chime.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ProfileController {
-	private final ProfileRepository repository;
+	private final ProfileService service;
 
 	@Autowired
-	public ProfileController(ProfileRepository repository) {
-		this.repository = repository;
-	}
-
-	public Profile getProfile(String id) {
-		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("id", "profile not found"));
+	public ProfileController(ProfileService service) {
+		this.service = service;
 	}
 }
--- server/src/main/java/io/github/shotoh/chime/profiles/ProfileService.java
+++ server/src/main/java/io/github/shotoh/chime/profiles/ProfileService.java
@@ -1,7 +1,19 @@
 package io.github.shotoh.chime.profiles;
 
+import io.github.shotoh.chime.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProfileService {
+	private final ProfileRepository repository;
+
+	@Autowired
+	public ProfileService(ProfileRepository repository) {
+		this.repository = repository;
+	}
+
+	public Profile getProfile(String id) {
+		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("id", "profile not found"));
+	}
 }
