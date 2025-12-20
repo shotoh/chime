5dd8475da6cbd00f13714d3d9800e3beb2c231e6
shotoh <trueshotoh@gmail.com>
2025-12-20 13:40:40 -0800

    remove constructors from records

--- server/src/main/java/io/github/shotoh/chime/profiles/Profile.java
+++ server/src/main/java/io/github/shotoh/chime/profiles/Profile.java
@@ -2,25 +2,8 @@ package io.github.shotoh.chime.profiles;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.github.shotoh.chime.sounds.Group;
-import java.security.SecureRandom;
-import java.time.Instant;
-import java.util.Base64;
 import java.util.UUID;
 import org.springframework.data.annotation.Id;
 
 public record Profile(@Id UUID id, @JsonIgnore String token, String name, long timestamp, Group rootGroup) {
-	public Profile() {
-		this(new Group());
-	}
-
-	public Profile(Group rootGroup) {
-		SecureRandom random = new SecureRandom();
-		Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
-
-		byte[] tokenBytes = new byte[32];
-		random.nextBytes(tokenBytes);
-		String token = encoder.encodeToString(tokenBytes);
-
-		this(UUID.randomUUID(), token, "New Profile", Instant.now().toEpochMilli(), rootGroup);
-	}
 }
