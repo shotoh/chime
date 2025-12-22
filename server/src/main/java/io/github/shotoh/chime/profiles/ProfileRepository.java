e4ebd3d5668e84fd911b42ec64f3698cef031e85
shotoh <trueshotoh@gmail.com>
2025-12-21 22:57:59 -0800

    remove repository comment

--- server/src/main/java/io/github/shotoh/chime/profiles/ProfileRepository.java
+++ server/src/main/java/io/github/shotoh/chime/profiles/ProfileRepository.java
@@ -1,9 +1,9 @@
 package io.github.shotoh.chime.profiles;
 
+import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProfileRepository extends CrudRepository<Profile, String> {
-	// Optional<Profile> findById(long id);
+public interface ProfileRepository extends CrudRepository<Profile, UUID> {
 }
