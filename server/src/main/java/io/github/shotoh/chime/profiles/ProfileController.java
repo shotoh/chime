6eb1ae0694fa5976e01048e6e3c8969059f35d7e
shotoh <trueshotoh@gmail.com>
2025-12-25 13:11:47 -0800

    impl update route

--- server/src/main/java/io/github/shotoh/chime/profiles/ProfileController.java
+++ server/src/main/java/io/github/shotoh/chime/profiles/ProfileController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -45,6 +46,13 @@ public class ProfileController {
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public Response deleteProfile(@PathVariable UUID id, @RequestHeader("Authorization") String token) {
+		service.deleteProfile(id, token);
+		return Success.success(null);
+	}
+
+	@PatchMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Response updateProfile(@PathVariable UUID id, @RequestHeader("Authorization") String token) {
 		service.deleteProfile(id, token);
 		return Success.success(null);
 	}
