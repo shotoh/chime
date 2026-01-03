084714959ec582c866e4cc373383d05250ee1811
shotoh <trueshotoh@gmail.com>
2026-01-03 01:05:08 -0800

    fix schema format

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,9 +1,5 @@
 CREATE TABLE IF NOT EXISTS profiles (
-                                        id
-                                        VARCHAR
-(
-                                        36
-) PRIMARY KEY,
+    id VARCHAR(36) PRIMARY KEY,
     token VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     last_updated BIGINT NOT NULL,
