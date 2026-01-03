56af8511e80604c4f71b2a8a17bb02eda5dba299
shotoh <trueshotoh@gmail.com>
2026-01-02 23:37:36 -0800

    organize schema

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,18 +1,7 @@
-CREATE TABLE IF NOT EXISTS profiles
-(
-    id
-    BINARY
-(
-    16
-) PRIMARY KEY,
-    token VARCHAR
-(
-    255
-) NOT NULL,
-    name VARCHAR
-(
-    255
-) NOT NULL,
+CREATE TABLE IF NOT EXISTS profiles (
+    id BINARY(16) PRIMARY KEY,
+    token VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     last_updated BIGINT NOT NULL,
     root_group JSON NOT NULL
-    );
+);
