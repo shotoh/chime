f2f3581b9d04d1a0f3d7e8e071d76285f6c94706
shotoh <trueshotoh@gmail.com>
2026-01-03 19:08:23 -0800

    add seed to sound

--- server/src/main/java/io/github/shotoh/chime/profiles/objects/Sound.java
+++ server/src/main/java/io/github/shotoh/chime/profiles/objects/Sound.java
@@ -4,5 +4,5 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 
 public record Sound(String soundId, @Min(0) int delay, @Min(0) @Max(2) float volume, @Min(0) @Max(2) float pitch,
-                    boolean enabled) implements GroupItem {
+                    @Min(0) int seed, boolean enabled) implements GroupItem {
 }
